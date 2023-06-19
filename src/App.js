a842f6ea5e6ce457e846ac9aa21f8458b1ca6409
BEARSUM <125293472+BEARSUM@users.noreply.github.com>
2023-06-19 18:17:17 +0900

    Add: test2

--- src/App.js
+++ src/App.js
@@ -3,6 +3,21 @@ import "./App.css";
 import React, { useState } from "react";
 
 function App() {
+  //test1
+  // const [text, setText] = useState();
+  // const [list, setList] = useState([]);
+
+  // function onChange(e) {
+  //   setText(e.target.value);
+  // }
+  // function onSave(e) {
+  //   e.preventDefault();
+  //   console.log(text);
+  //   const newList = text;
+  //   setList([...list, newList]);
+  // }
+
+  //test2
   const [text, setText] = useState();
   const [list, setList] = useState([]);
 
@@ -13,19 +28,35 @@ function App() {
     e.preventDefault();
     console.log(text);
     const newList = text;
-    setList([...list, newList]);
+
+    if (newList !== "") setList([...list, newList]);
   }
   return (
     <div className="App">
-      <input id="input" onChange={onChange}></input>
-      <button className="btn" onClick={onSave}>
-        저장!
-      </button>
-      {list.map((el) => (
-        <ul>
-          <li>{el}</li>
-        </ul>
-      ))}
+      {/* <div className="test_1">
+        TEST-1
+        <input onChange={onChange}></input>
+        <button className="btn" onClick={onSave}>
+          저장!
+        </button>
+        {list.map((el) => (
+          <ul>
+            <li>{el}</li>
+          </ul>
+        ))}
+      </div> */}
+      <div className="test_2">
+        TEST-2
+        <input onChange={onChange}></input>
+        <button className="btn" onClick={onSave}>
+          저장!
+        </button>
+        {list.map((el) => (
+          <ul>
+            <li>{el}</li>
+          </ul>
+        ))}
+      </div>
     </div>
   );
 }
