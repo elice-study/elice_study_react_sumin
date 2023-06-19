3ef7e378ed8186ccd1aa08344671f2b8e98b483d
BEARSUM <125293472+BEARSUM@users.noreply.github.com>
2023-06-19 13:55:34 +0900

    Add : input list

--- src/App.js
+++ src/App.js
@@ -3,16 +3,29 @@ import "./App.css";
 import React, { useState } from "react";
 
 function App() {
-  const [text, setText] = useState("");
-  const onChange = (e) => {
-    setText(e.target.value);
-  };
+  const [text, setText] = useState();
+  const [list, setList] = useState([]);
 
+  function onChange(e) {
+    setText(e.target.value);
+  }
+  function onSave(e) {
+    e.preventDefault();
+    console.log(text);
+    const newList = text;
+    setList([...list, newList]);
+  }
   return (
     <div className="App">
-      <input onChange={onChange} value={text}></input>
-      <button className="btn" onClick={onChange}></button>
-      <div className="div">값: {text}</div>
+      <input id="input" onChange={onChange}></input>
+      <button className="btn" onClick={onSave}>
+        저장!
+      </button>
+      {list.map((el) => (
+        <ul>
+          <li>{el}</li>
+        </ul>
+      ))}
     </div>
   );
 }
