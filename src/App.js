ae4f429fd196a6767d4dcb746b47f7b4354f29b5
BEARSUM <125293472+BEARSUM@users.noreply.github.com>
2023-06-21 01:17:18 +0900

    ADD: test3
    fetch data after 2 seconds.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function App() {
   //test1
@@ -18,19 +18,38 @@ function App() {
   // }
 
   //test2
-  const [text, setText] = useState();
-  const [list, setList] = useState([]);
+  // const [text, setText] = useState();
+  // const [list, setList] = useState([]);
 
-  function onChange(e) {
-    setText(e.target.value);
-  }
-  function onSave(e) {
-    e.preventDefault();
-    console.log(text);
-    const newList = text;
+  // function onChange(e) {
+  //   setText(e.target.value);
+  // }
+  // function onSave(e) {
+  //   console.log(text);
+  //   const newList = text;
 
-    if (newList !== "") setList([...list, newList]);
+  //   if (newList !== "") setList([...list, newList]);
+  // }
+
+  //test3
+  //getData 첫 랜더링시 한번만 실행
+  useEffect(() => {
+    getData();
+  }, []);
+  const [load, setLoad] = useState(false);
+  const [data, setData] = useState([]);
+  function getData() {
+    setTimeout(() => {
+      fetch("https://jsonplaceholder.typicode.com/albums/1/photos")
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          setData(data);
+        });
+      setLoad(true);
+    }, 2000);
   }
+
   return (
     <div className="App">
       {/* <div className="test_1">
@@ -45,7 +64,7 @@ function App() {
           </ul>
         ))}
       </div> */}
-      <div className="test_2">
+      {/* <div className="test_2">
         TEST-2
         <input onChange={onChange}></input>
         <button className="btn" onClick={onSave}>
@@ -56,6 +75,21 @@ function App() {
             <li>{el}</li>
           </ul>
         ))}
+      </div> */}
+
+      <div>
+        {load ? (
+          <div>
+            {data.map((item) => (
+              <div key={item.id}>
+                <h4>`TITLE : {item.title}`</h4>
+                <img src={item.url} alt="" />
+              </div>
+            ))}
+          </div>
+        ) : (
+          <span>로딩중...</span>
+        )}
       </div>
     </div>
   );
